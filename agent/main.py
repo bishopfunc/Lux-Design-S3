843c90a5349c7423944fd03aff2b0fa1a7ca2807
bishopfunc <showyamamoto02@gmail.com>
2025-02-17 03:13:01 +0900

    update

--- agent/main.py
+++ agent/main.py
@@ -2,7 +2,7 @@ import json
 from typing import Dict
 import sys
 from argparse import Namespace
-
+import os
 import numpy as np
 
 from agent import Agent
@@ -22,11 +22,22 @@ def agent_fn(observation, configurations):
     step = observation.step
     player = observation.player
     remainingOverageTime = observation.remainingOverageTime
+
     if step == 0:
         agent_dict[player] = Agent(player, configurations["env_cfg"])
+        
+    if "__raw_path__" in configurations:
+        dirname = os.path.dirname(configurations["__raw_path__"])
+    else:
+        dirname = os.path.dirname(__file__)
+
+    sys.path.append(os.path.abspath(dirname))
+
     agent = agent_dict[player]
     actions = agent.act(step, from_json(obs), remainingOverageTime)
     return dict(action=actions.tolist())
+
+
 
 if __name__ == "__main__":
     
